54a3bf6fb676e3f4b8bf17fd8d215086e65c27d6
TheFriedHashbrown <amygreat0@gmail.com>
2024-12-19 17:05:01 +0530

    Added input to pacman

--- main.lua
+++ main.lua
@@ -6,6 +6,8 @@ function love.load()
     _G.pacman = {}
     pacman.x = 200
     pacman.y = 250
+    pacman.angle1 = 1
+    pacman.angle2 = 5
 
     _G.food = {}
     food.x = 600
@@ -13,7 +15,23 @@ function love.load()
 end
 
 function love.update(dt)
-    pacman.x = pacman.x + 1
+    if love.keyboard.isDown("down") then
+        pacman.angle1 = pacman.angle1 + math.pi * dt
+        pacman.angle2 = pacman.angle2 + math.pi * dt
+    elseif love.keyboard.isDown("up") then
+        pacman.angle1 = pacman.angle1 - math.pi * dt
+        pacman.angle2 = pacman.angle2 - math.pi * dt
+    end
+
+    if love.keyboard.isDown("a") then
+        pacman.x = pacman.x - 1
+    elseif love.keyboard.isDown("d") then
+        pacman.x = pacman.x + 1    
+    elseif love.keyboard.isDown("w") then
+        pacman.y = pacman.y - 1    
+    elseif love.keyboard.isDown("s") then
+        pacman.y = pacman.y + 1        
+    end
 
     if pacman.x >= food.x + 15 then
         food.eaten = true
@@ -27,6 +45,6 @@ function love.draw()
     end
 
     love.graphics.setColor(1, 0.7, 0.1)
-    love.graphics.arc("fill", pacman.x, pacman.y, 60, 1, 5)
+    love.graphics.arc("fill", pacman.x, pacman.y, 60, pacman.angle1, pacman.angle2)
     
 end 
